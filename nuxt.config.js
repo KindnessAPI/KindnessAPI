32233f76290b0c82dbe80a87d274a2b138952cda
Wong Lok <yellowhappy831@gmail.com>
2019-02-06 08:03:16 +0800

    heroku deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Kindness API | Spread Kidness with APIs',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Trying to make Kindness Popular with Rich Interactions and Visuals' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
